9ea5e48644b82616b338d0f3dca00a575d79a758
Mehthab <mehthabunstoppable@gmail.com>
2025-05-08 22:07:15 +0530

    Update script.js

--- script.js
+++ script.js
@@ -53,28 +53,46 @@
             item.style.opacity = 0;
             item.style.left = '0';
         });
+
+        items[active - 2 >= 0 ? active - 2 : items.length-2].style.transform = `none`;
+        items[active - 2 >= 0 ? active - 2 : items.length-2].style.zIndex = 1;
+        items[active - 2 >= 0 ? active - 2 : items.length-2].style.filter = 'none';  
+        items[active - 2 >= 0 ? active - 2 : items.length-2].style.zIndex = 15;
+        items[active - 2 >= 0 ? active - 2 : items.length-2].style.left = '5vw';
+        items[active - 2 >= 0 ? active - 2 : items.length-2].style.transform = `translate(-200%)`;
+        
         
         items[active - 1 >= 0 ? active - 1 : items.length-1].style.transform = `none`;
         items[active - 1 >= 0 ? active - 1 : items.length-1].style.zIndex = 1;
         items[active - 1 >= 0 ? active - 1 : items.length-1].style.filter = 'none';
-        items[active - 1 >= 0 ? active - 1 : items.length-1].style.opacity = 1;
         items[active - 1 >= 0 ? active - 1 : items.length-1].style.zIndex = 15;
         items[active - 1 >= 0 ? active - 1 : items.length-1].style.left = '5vw';
         items[active - 1 >= 0 ? active - 1 : items.length-1].style.transform = `translate(-100%)`;
+        items[active - 1 >= 0 ? active - 1 : items.length-1].style.opacity = 1;
 
         items[active].style.transform = `none`;
         items[active].style.zIndex = 1;
         items[active].style.filter = 'none';
-        items[active].style.opacity = 1;
+        
         items[active].style.left = '10vw';
         items[active].style.zIndex = 15;
+        items[active].style.opacity = 1;
 
-        items[active + 1 < items.length ? active + 1 : 0].style.transform = `none`;
-        items[active + 1 < items.length ? active + 1 : 0].style.zIndex = 1;
-        items[active + 1 < items.length ? active + 1 : 0].style.filter = 'none';
-        items[active + 1 < items.length ? active + 1 : 0].style.opacity = 1;
-        items[active + 1 < items.length ? active + 1 : 0].style.zIndex = 15;
-        items[active + 1 < items.length ? active + 1 : 0].style.left = '95vw';
+        items[active + 1 < items.length ? active + 1 : active + 1 - items.length].style.transform = `none`;
+        items[active + 1 < items.length ? active + 1 : active + 1 - items.length].style.zIndex = 1;
+        items[active + 1 < items.length ? active + 1 : active + 1 - items.length].style.filter = 'none';
+        items[active + 1 < items.length ? active + 1 : active + 1 - items.length].style.zIndex = 15;
+        items[active + 1 < items.length ? active + 1 : active + 1 - items.length].style.left = '95vw';
+        items[active + 1 < items.length ? active + 1 : active + 1 - items.length].style.opacity = 1;
+
+        
+
+        items[active + 2 < items.length ? active + 2 : active + 2 - items.length].style.transform = `none`;
+        items[active + 2 < items.length ? active + 2 : active + 2 - items.length].style.zIndex = 1;
+        items[active + 2 < items.length ? active + 2 : active + 2 - items.length].style.filter = 'none';
+        items[active + 2 < items.length ? active + 2 : active + 2 - items.length].style.zIndex = 15;
+        items[active + 2 < items.length ? active + 2 : active + 2 - items.length].style.left = '195vw';
+        
         
       
 
