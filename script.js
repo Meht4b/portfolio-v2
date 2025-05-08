f5b7b1e34244b2b8af1774808b3ab290156c990e
Mehthab <mehthabunstoppable@gmail.com>
2025-05-08 22:32:16 +0530

    final carousel

--- script.js
+++ script.js
@@ -1,111 +1,68 @@
-/*let items = document.querySelectorAll('.projects-inner-cont .project');
-    let next = document.querySelector('.proj-btn-left');
-    let prev = document.querySelector('.proj-btn-right');
+
+let items = document.querySelectorAll('.projects-inner-cont .project');
+let next = document.querySelector('.proj-btn-right');
+let prev = document.querySelector('.proj-btn-left');
+
+let active = 0;
+function loadShow(){
+    let stt = 0;
+
+    items.forEach(item => {
+        item.style.transform = 'none';
+        item.style.zIndex = -1;
+        item.style.filter = 'none';
+        item.style.opacity = 0;
+        item.style.left = '0';
+    });
+
+    items[active - 2 >= 0 ? active - 2 : items.length-2].style.transform = `none`;
+    items[active - 2 >= 0 ? active - 2 : items.length-2].style.zIndex = 1;
+    items[active - 2 >= 0 ? active - 2 : items.length-2].style.filter = 'none';  
+    items[active - 2 >= 0 ? active - 2 : items.length-2].style.zIndex = 15;
+    items[active - 2 >= 0 ? active - 2 : items.length-2].style.left = '5vw';
+    items[active - 2 >= 0 ? active - 2 : items.length-2].style.transform = `translate(-200%)`;
     
-    let active = 3;
-    function loadShow(){
-        let stt = 0;
-        items[active].style.transform = `none`;
-        items[active].style.zIndex = 1;
-        items[active].style.filter = 'none';
-        items[active].style.opacity = 1;
-        for(var i = active + 1; i < items.length; i++){
-            stt++;
-            items[i].style.transform = `translateX(${120*stt}px) scale(${1 - 0.2*stt}) perspective(16px) rotateY(-1deg)`;
-            items[i].style.zIndex = -stt;
-            items[i].style.filter = 'blur(5px)';
-            items[i].style.opacity = stt > 2 ? 0 : 0.6;
-        }
-        stt = 0;
-        for(var i = active - 1; i >= 0; i--){
-            stt++;
-            items[i].style.transform = `translateX(${-120*stt}px) scale(${1 - 0.2*stt}) perspective(16px) rotateY(1deg)`;
-            items[i].style.zIndex = -stt;
-            items[i].style.filter = 'blur(5px)';
-            items[i].style.opacity = stt > 2 ? 0 : 0.6;
-        }
-    }
+    
+    items[active - 1 >= 0 ? active - 1 : items.length-1].style.transform = `none`;
+    items[active - 1 >= 0 ? active - 1 : items.length-1].style.zIndex = 1;
+    items[active - 1 >= 0 ? active - 1 : items.length-1].style.filter = 'none';
+    items[active - 1 >= 0 ? active - 1 : items.length-1].style.zIndex = 15;
+    items[active - 1 >= 0 ? active - 1 : items.length-1].style.left = '5vw';
+    items[active - 1 >= 0 ? active - 1 : items.length-1].style.transform = `translate(-100%)`;
+    items[active - 1 >= 0 ? active - 1 : items.length-1].style.opacity = 1;
+
+    items[active].style.transform = `none`;
+    items[active].style.zIndex = 1;
+    items[active].style.filter = 'none';
+    
+    items[active].style.left = '10vw';
+    items[active].style.zIndex = 15;
+    items[active].style.opacity = 1;
+
+    items[active + 1 < items.length ? active + 1 : active + 1 - items.length].style.transform = `none`;
+    items[active + 1 < items.length ? active + 1 : active + 1 - items.length].style.zIndex = 1;
+    items[active + 1 < items.length ? active + 1 : active + 1 - items.length].style.filter = 'none';
+    items[active + 1 < items.length ? active + 1 : active + 1 - items.length].style.zIndex = 15;
+    items[active + 1 < items.length ? active + 1 : active + 1 - items.length].style.left = '95vw';
+    items[active + 1 < items.length ? active + 1 : active + 1 - items.length].style.opacity = 1;
+
+    
+
+    items[active + 2 < items.length ? active + 2 : active + 2 - items.length].style.transform = `none`;
+    items[active + 2 < items.length ? active + 2 : active + 2 - items.length].style.zIndex = 1;
+    items[active + 2 < items.length ? active + 2 : active + 2 - items.length].style.filter = 'none';
+    items[active + 2 < items.length ? active + 2 : active + 2 - items.length].style.zIndex = 15;
+    items[active + 2 < items.length ? active + 2 : active + 2 - items.length].style.left = '195vw';
+    
+}
+
+loadShow();
+next.onclick = function(){
+    active = active + 1 < items.length ? active + 1 : 0;
     loadShow();
-    next.onclick = function(){
-        active = active + 1 < items.length ? active + 1 : active;
-        loadShow();
-    }
-    prev.onclick = function(){
-        active = active - 1 >= 0 ? active - 1 : active;
-        loadShow();
-    }
-
-    */
-
-
-    let items = document.querySelectorAll('.projects-inner-cont .project');
-    let next = document.querySelector('.proj-btn-right');
-    let prev = document.querySelector('.proj-btn-left');
-    
-    let active = 0;
-    function loadShow(){
-        let stt = 0;
-
-        items.forEach(item => {
-            item.style.transform = 'none';
-            item.style.zIndex = -1;
-            item.style.filter = 'none';
-            item.style.opacity = 0;
-            item.style.left = '0';
-        });
-
-        items[active - 2 >= 0 ? active - 2 : items.length-2].style.transform = `none`;
-        items[active - 2 >= 0 ? active - 2 : items.length-2].style.zIndex = 1;
-        items[active - 2 >= 0 ? active - 2 : items.length-2].style.filter = 'none';  
-        items[active - 2 >= 0 ? active - 2 : items.length-2].style.zIndex = 15;
-        items[active - 2 >= 0 ? active - 2 : items.length-2].style.left = '5vw';
-        items[active - 2 >= 0 ? active - 2 : items.length-2].style.transform = `translate(-200%)`;
-        
-        
-        items[active - 1 >= 0 ? active - 1 : items.length-1].style.transform = `none`;
-        items[active - 1 >= 0 ? active - 1 : items.length-1].style.zIndex = 1;
-        items[active - 1 >= 0 ? active - 1 : items.length-1].style.filter = 'none';
-        items[active - 1 >= 0 ? active - 1 : items.length-1].style.zIndex = 15;
-        items[active - 1 >= 0 ? active - 1 : items.length-1].style.left = '5vw';
-        items[active - 1 >= 0 ? active - 1 : items.length-1].style.transform = `translate(-100%)`;
-        items[active - 1 >= 0 ? active - 1 : items.length-1].style.opacity = 1;
-
-        items[active].style.transform = `none`;
-        items[active].style.zIndex = 1;
-        items[active].style.filter = 'none';
-        
-        items[active].style.left = '10vw';
-        items[active].style.zIndex = 15;
-        items[active].style.opacity = 1;
-
-        items[active + 1 < items.length ? active + 1 : active + 1 - items.length].style.transform = `none`;
-        items[active + 1 < items.length ? active + 1 : active + 1 - items.length].style.zIndex = 1;
-        items[active + 1 < items.length ? active + 1 : active + 1 - items.length].style.filter = 'none';
-        items[active + 1 < items.length ? active + 1 : active + 1 - items.length].style.zIndex = 15;
-        items[active + 1 < items.length ? active + 1 : active + 1 - items.length].style.left = '95vw';
-        items[active + 1 < items.length ? active + 1 : active + 1 - items.length].style.opacity = 1;
-
-        
-
-        items[active + 2 < items.length ? active + 2 : active + 2 - items.length].style.transform = `none`;
-        items[active + 2 < items.length ? active + 2 : active + 2 - items.length].style.zIndex = 1;
-        items[active + 2 < items.length ? active + 2 : active + 2 - items.length].style.filter = 'none';
-        items[active + 2 < items.length ? active + 2 : active + 2 - items.length].style.zIndex = 15;
-        items[active + 2 < items.length ? active + 2 : active + 2 - items.length].style.left = '195vw';
-        
-        
-      
-
-
-    }
+}
+prev.onclick = function(){
+    active = active - 1 >= 0 ? active - 1 : items.length-1;
     loadShow();
-    next.onclick = function(){
-        active = active + 1 < items.length ? active + 1 : 0;
-        loadShow();
-    }
-    prev.onclick = function(){
-        active = active - 1 >= 0 ? active - 1 : items.length-1;
-        loadShow();
-    }
+}
 
-    
